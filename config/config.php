8ec468eb9b30f9d2d78acc1db34eb47c9d237ef8
Roshan Timalsina <timalsinaroshan345@gmail.com>
2026-07-14 20:24:21 +0545

    update

--- config/config.php
+++ config/config.php
@@ -1,5 +1,10 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
+    $sessionPath = __DIR__ . '/../storage/sessions';
+    if (!is_dir($sessionPath)) {
+        mkdir($sessionPath, 0775, true);
+    }
+    session_save_path($sessionPath);
     session_start();
 }
 
